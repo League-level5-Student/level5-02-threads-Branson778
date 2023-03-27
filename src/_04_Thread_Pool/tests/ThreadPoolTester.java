62122cd12010ad42b6a55efc752e299af2b95d44
r_boo <r_boo@192.168.1.84>
2023-03-26 18:21:42 -0700

    Broken At Step 70

--- src/_04_Thread_Pool/tests/ThreadPoolTester.java
+++ src/_04_Thread_Pool/tests/ThreadPoolTester.java
@@ -3,11 +3,18 @@ package _04_Thread_Pool.tests;
 public class ThreadPoolTester {
 public static void main(String[] args) {
 	WorkQueue wq = new WorkQueue();
-	System.out.println("Total threads: "+wq.getThreadCount());
-	try {
-		Thread.sleep(1000);
-	} catch (InterruptedException e) {
-		e.printStackTrace();
+//	System.out.println("Total threads: "+wq.getThreadCount());
+	//try {
+	//	Thread.sleep(1000);
+	//} catch (InterruptedException e) {
+	//	e.printStackTrace();
+//	}
+	for(int i=0; i <1000; i++) {
+		int x = i;
+		Job j = () ->{
+			System.out.println("Printing "+x+" from thread #" + Thread.currentThread().getId());
+		};
+		wq.addJob(j);
 	}
 	wq.shutdown();
 }
