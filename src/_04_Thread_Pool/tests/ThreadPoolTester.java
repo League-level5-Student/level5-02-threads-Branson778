ff09ce7eb73880f2fc559cf7550d9902f6bfdcc1
r_boo <r_boo@DESKTOP-61FSOOO.attlocal.net>
2023-04-09 17:00:30 -0700

    Distance From Center Test - Small Errors Swimming - Get Help Starting

--- src/_04_Thread_Pool/tests/ThreadPoolTester.java
+++ src/_04_Thread_Pool/tests/ThreadPoolTester.java
@@ -3,12 +3,12 @@ package _04_Thread_Pool.tests;
 public class ThreadPoolTester {
 public static void main(String[] args) {
 	WorkQueue wq = new WorkQueue();
-//	System.out.println("Total threads: "+wq.getThreadCount());
-	//try {
-	//	Thread.sleep(1000);
-	//} catch (InterruptedException e) {
-	//	e.printStackTrace();
-//	}
+	System.out.println("Total threads: "+wq.getThreadCount());
+	try {
+		Thread.sleep(1000);
+	} catch (InterruptedException e) {
+		e.printStackTrace();
+	}
 	for(int i=0; i <1000; i++) {
 		int x = i;
 		Job j = () ->{
